807ed8bd47c4ccec0ba2feaaa061c79d3bb83d56
ilya <ilya.vas@outlook.com>
2023-11-08 09:56:01 +0200

    review model added

--- permanent_makeup/main/models.py
+++ permanent_makeup/main/models.py
@@ -33,6 +33,15 @@ class WorkGalllery(models.Model):
 
     def __str__(self):
         return f"{self.title} at {self.created_at}"
+    
+class Review(models.Model):
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='reviews')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review by {self.author.username} on {self.created_at.strftime('%Y-%m-%d')}"
 
 
 class Reservation(models.Model):

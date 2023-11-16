548bb675d23ee5ccbb4cd1f5434c66537913fe73
ilya <ilya.vas@outlook.com>
2023-11-16 15:32:24 +0200

    logging modified

--- permanent_makeup/permanent_makeup/settings.py
+++ permanent_makeup/permanent_makeup/settings.py
@@ -29,18 +29,13 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'debug.log',
+        'console': {
+            'class': 'logging.StreamHandler',
         },
     },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
     },
 }
 
